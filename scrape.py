d90d8ec1afeafd2235c4644ab4a092aeedcb8fef
LordExodius <oscar45697@gmail.com>
2022-07-15 21:58:26 -0400

    updated scrape.py andy waz rite

--- scrape.py
+++ scrape.py
@@ -1,25 +1,35 @@
 # COURSE SCRAPER
 # Oscar Yu 2022 ©
 
+ANIMATE = True
+SKIP = False
+
 from bs4 import BeautifulSoup
+import psycopg2
 import requests
-import json
 import re
 import time
+import pandas as pd
+import os
+from dotenv import load_dotenv
+import csv
+load_dotenv()
 
-ANIMATE = True
+status = "Loading"
 
+# Record start time
 startTime = time.time()
 
+# Animation
 if ANIMATE:
     import itertools
     import threading
     import sys
     def animate():
-        for c in itertools.cycle(['|', '/', '-', '\\']):
+        for c in itertools.cycle(['-', '\\', '|', '/']):
             if done:
                 break
-            sys.stdout.write('\rloading ' + c + f' time elapsed: {round(time.time() - startTime, 2)} seconds')
+            sys.stdout.write(f'\r{status} {c}' + f' Time elapsed: {round(time.time() - startTime, 2)} seconds')
             sys.stdout.flush()
             time.sleep(0.1)
         sys.stdout.write('\rDone!     ')
@@ -29,23 +39,58 @@ if ANIMATE:
     t.daemon=True
     t.start()
 
+# Load database connection data from env
+hostname = os.environ.get("DB_HOST_NAME")
+port = os.environ.get("DB_PORT")
+dbName = os.environ.get("DB_NAME")
+user = os.environ.get("DB_USER_NAME")
+password = os.environ.get("DB_PASSWORD")
+
+status = "Connecting"
+# Create postgres connection
+conn = psycopg2.connect(
+    host = hostname,
+    port = port,
+    database = dbName,
+    user = user,
+    password = password
+)
+
+# Verify connection
+curr = conn.cursor()
+curr.execute("SELECT version();")
+dbVersion = curr.fetchone()
+curr.close()
+print(f"\n{dbVersion[0]}\n")
+
+# Import categories
 file = open("categories.txt", "r")
 catList = [line.rstrip("\n") for line in file.readlines()]
 file.close()
-#print(catList)
 
-newFile = open("output1.json", "w")
+# Create pandas dataframe
+courses = pd.DataFrame(columns=[
+    "course_code", 
+    "course_name", 
+    "antirequisites", 
+    "prerequisites", 
+    "description",
+    "location",
+    "extra_info"])
 
-courseList = []
+scrapeCount = 0
+status = "Scraping"
+
 for cat in catList:
+    if SKIP:
+        break
     url = f"https://www.westerncalendar.uwo.ca/Courses.cfm?Subject={cat}&SelectedCalendar=Live&ArchiveID="
 
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     for panel in soup.find_all(class_="panel-default")[2:]:
-        courseDict = {}
         title = panel.find_all(class_="courseTitleNoBlueLink")[0].get_text().strip()
-        desc = panel.find_all(class_="col-xs-12")[0].get_text().strip()
+        desc = panel.find_all(class_="col-xs-12")[0].get_text().strip().replace("\n", "").replace("\r", "")
         location = panel.find_all("img")[0]["alt"]
 
         # check for anti
@@ -56,11 +101,11 @@ for cat in catList:
             if child.find_all("strong"):
                 strong = child.find_all("strong")[0].get_text()
                 if strong == "Antirequisite(s):":
-                    antireqs = child.get_text().replace("Antirequisite(s):", "").strip()
+                    antireqs = child.get_text().replace("Antirequisite(s):", "").strip().replace("\n", "").replace("\r", "")
                 elif strong == "Prerequisite(s):":
-                    prereqs = child.get_text().replace("Prerequisite(s):", "").strip()
+                    prereqs = child.get_text().replace("Prerequisite(s):", "").strip().replace("\n", "").replace("\r", "")
                 elif strong == "Extra Information:":
-                    extra = child.get_text().replace("Extra Information:", "").strip()
+                    extra = child.get_text().replace("Extra Information:", "").strip().replace("\n", "").replace("\r", "")
 
         # parse title
         codePattern = re.compile(r"([0-9]{4}[A-Z/]*) ")
@@ -72,19 +117,43 @@ for cat in catList:
         code = code[0]
         name = re.findall(namePattern, title)[0] if re.findall(namePattern, title) else ""
         
-        courseDict["code"] = cat + " " + code
-        courseDict["name"] = name
-        courseDict["desc"] = desc
-        courseDict["anti"] = antireqs
-        courseDict["pre"] = prereqs
-        courseDict["extra"] = extra
-        courseDict["loc"] = location
-        # print(courseDict)
+        code = cat + " " + code
+
+        # look for existing code in dataframe
+        if code in courses["course_code"].values:
+            c = courses["course_code"]==code
+            if location == "Western Main Campus":
+                courses.loc[c, "prerequisites"] = prereqs.replace("\n", "")
+            courses.loc[c, "location"] += f",{location}"
+            continue
+
+        else:
+            row = pd.DataFrame({
+                "course_code":code,
+                "course_name":name, 
+                "antirequisites":antireqs,
+                "prerequisites":prereqs,
+                "description":desc,
+                "location":location,
+                "extra_info":extra}, index=[0])
+            courses = pd.concat([row,courses.loc[:]], axis = 0).reset_index(drop=True)
         # print()
-        courseList.append(courseDict)
-jsonDict = {"courses":courseList}
-json.dump(jsonDict, newFile)
-newFile.close()
+
+if not SKIP:
+    status = "Writing to CSV"
+    csvFile = open("output.csv", "w")
+    courses.to_csv(csvFile, sep="|", header=False, index=False, line_terminator="\n")
+    csvFile.close()
+
+status = "Writing to DB"
+csvFile = open("output.csv")
+curr = conn.cursor()
+curr.copy_from(csvFile, "courses", sep="|")
+
+csvFile.close()
+curr.close()
+conn.commit()
+conn.close()
 
 if ANIMATE:
     done = True
