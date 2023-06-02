9aa046e84a0a4a31e6bf0a73b2f0af4dab460adf
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2023-06-02 11:02:49 -0400

    scraper gets prereqs coreqs antireqs and precoreqs

--- prereqScraper.py
+++ prereqScraper.py
@@ -17,6 +17,7 @@ courses = pd.DataFrame(columns=[
     "extra_info"])
 
 for cat in catList:
+    print(cat)
     if SKIP_SCRAPE:
         break
     url = f"https://www.westerncalendar.uwo.ca/Courses.cfm?Subject={cat}&SelectedCalendar=Live&ArchiveID="
@@ -26,50 +27,61 @@ for cat in catList:
     for panel in soup.find_all(class_="panel-default")[2:]:
         prereqs = []  # [string, link?, bold?]
         antireqs = []
+        coreqs = []
+        preAndCoreqs = []
+        coreqFlag = False
+        pcreqFlag = False
         title = panel.find_all(class_="courseTitleNoBlueLink")[
             0].get_text().strip()
-        print(title)
         desc = panel.find_all(
             class_="col-xs-12")[0].get_text().strip().replace("\n", "").replace("\r", "")
         location = panel.find_all("img")[0]["alt"]
         for child in panel.find_all(class_="col-xs-12"):
             if child.find_all("strong"):
-
-                # if child.find_all("strong")[0].get_text() == "Antirequisite(s):":
-                #     print("="*20, "ANTIREQS")
-                #     for item in child.children:
-                #         print(item)
-                #         print("*"*20)
                 if child.find_all("strong")[0].get_text() == "Antirequisite(s):":
                     for item in child.children:
                         if hasattr(item, "children"):
                             for req in item.children:
                                 reqTraits = [req.get_text().strip().replace(
-                                    "\n", "").replace("\r", ""), False, False]
+                                    "\n", "").replace("\r", ""), False]
                                 if not reqTraits[0]:
                                     continue
                                 if type(req) == Tag:
                                     if req.name == "a":
                                         reqTraits[1] = True
-                                    elif (req.name == "strong"):
-                                        reqTraits[2] = True
                                 antireqs.append(reqTraits)
-                if child.find_all("strong")[0].get_text() == "Prerequisite(s):":
+                elif child.find_all("strong")[0].get_text() == "Prerequisite(s):":
                     for item in child.children:
                         if hasattr(item, "children"):
                             for req in item.children:
                                 reqTraits = [req.get_text().strip().replace(
-                                    "\n", "").replace("\r", ""), False, False]
+                                    "\n", "").replace("\r", ""), False]
                                 if not reqTraits[0]:
                                     continue
+                                if "Prerequisite(s):" in reqTraits[0]:
+                                    continue
+                                if "Pre-or Corequisite(s)" in reqTraits[0]:
+                                    pcreqFlag = True
+                                elif "Corequisite(s)" in reqTraits[0]:
+                                    coreqFlag = True
                                 if type(req) == Tag:
                                     if req.name == "a":
                                         reqTraits[1] = True
-                                    elif (req.name == "strong"):
-                                        reqTraits[2] = True
-                                prereqs.append(reqTraits)
-        print(prereqs)
-        print("="*20)
-        print(antireqs)
-        print("="*20)
-    break
+                                if pcreqFlag and coreqFlag:
+                                    print(title + " EXCEPTION")
+                                    raise Exception("BOTH FLAGS ACTIVE")
+                                elif pcreqFlag:
+                                    preAndCoreqs.append(reqTraits)
+                                elif coreqFlag:
+                                    coreqs.append(reqTraits)
+                                else:
+                                    prereqs.append(reqTraits)
+        # if antireqs:
+        #     print("ANTI", antireqs)
+        # if prereqs:
+        #     print("PRE", prereqs)
+        # if coreqs:
+        #     print("CO", coreqs)
+        # if preAndCoreqs:
+        #     print("PRECO", preAndCoreqs)
+        # print("="*20)
